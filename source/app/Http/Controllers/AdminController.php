b3f183ee2fc37a7bcc50b2c541bad3387dc13744
FrostyJet <edhovhannisyan97@gmail.com>
2021-05-07 13:26:19 +0400

    ...

---
+++ source/app/Http/Controllers/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function renderHome()
+    {
+        return view('admin.home');
+    }
+}
--- source/app/Http/Controllers/HomeController.php
+++ source/app/Http/Controllers/HomeController.php
@@ -23,6 +23,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $data = [];
+        $data['features'] = \App\Models\Defines::getFeatures();
+
+        return view('home', $data);
     }
 }
