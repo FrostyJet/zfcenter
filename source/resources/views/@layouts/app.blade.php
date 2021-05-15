aedcd6679529dfb7618284a4c63f63cef564fa3f
FrostyJet <edhovhannisyan97@gmail.com>
2021-05-15 12:11:11 +0400

    ...

--- source/resources/views/@layouts/app.blade.php
+++ source/resources/views/@layouts/app.blade.php
@@ -2,11 +2,21 @@
 <html lang="en">
 
 <head>
-    
     <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta charset="utf-8">
+    <meta property="og:image" content="{{URL::to('/')}}/img/i/logo_1.png">
+    <meta property="og:type" content="article">
+    <meta property="og:url" content="{{URL::to('/')}}/">
+    <link rel="shortcut icon" href="{{URL::to('/')}}/img/favicon.ico" type="image/vnd.microsoft.icon">
+    <meta property="og:site_name" content="ZF Center">
+    <meta property="og:description" content="Бесплатная диагностика, ремонт АКПП․․․">
+    <meta property="og:title" content="ZF Center | Армения">
+
+    <meta name="description" content="Бесплатная диагностика и ремонт АКПП в Москве за 2-3 дня! Гарантия на ремонт 6-12 месяцев. Бесплатный эвакуатор по Москве.">
+    <meta name="keywords" content="диагностика и ремонт акпп, диагностика и ремонт акпп в москве, бесплатная диагностика акпп">
 
     <link rel="preconnect" href="{{URL::to('/')}}">
     <link rel="shortcut icon" href="/img/favicon.ico" type="image/vnd.microsoft.icon">
